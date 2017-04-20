ad398bc0d160eb5ea6cda0bde96a2151933ab331
Zachary Rump <zrrm74@mail.missouri.edu>
2017-04-19 22:42:28 -0500

    Connect switches to test bench.

--- Lab7Bench.vhd
+++ Lab7Bench.vhd
@@ -2,8 +2,8 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity Lab7Bench is
-  port(DIN : in std_logic_vector(7 downto 0) := (others => '0');
-       LD1, LD2, CLK : in std_logic := '0';
+  port(SW0, SW1, SW2, SW3, SW4, SW5, SW6, SW7 : in std_logic;
+       BTN0, BTN1, CLK : in std_logic;
        AN0, AN1, AN2, AN3 : out std_logic := '0';
        segment_a, segment_b, segment_c, segment_d, segment_e, segment_f, segment_g, segment_dp : out std_logic := '0');
 end Lab7Bench;
@@ -25,6 +25,7 @@ architecture Behavior of Lab7Bench is
         segment_a, segment_b, segment_c, segment_d, segment_e, segment_f, segment_g : out std_logic);
   end component;
 
+  signal number_in : std_logic_vector(7 downto 0);
   signal number_out : std_logic_vector(15 downto 0);
   signal done_signal : std_logic;
   signal counter : std_logic_vector(2 downto 0);
@@ -36,12 +37,20 @@ begin
   AN1 <= Anode(1);
   AN2 <= Anode(2);
   AN3 <= Anode(3);
-
+  number_in(0) <= SW0;
+  number_in(1) <= SW1;
+  number_in(2) <= SW2;
+  number_in(3) <= SW3;
+  number_in(4) <= SW4;
+  number_in(5) <= SW5;
+  number_in(6) <= SW6;
+  number_in(7) <= SW7;
+  
   segment_dp <= '1';
 
   -- Named association component name => entity name
-  -- CONV: BCD2BIN port map()
-  -- CONV: BIN2BCD port map()
+  -- CONV0: BCD2BIN port map(number_in, BTN0, BTN1, CLK, number_out)
+  -- CONV1: BIN2BCD port map(number_in, BTN0, BTN1, CLK, number_out)
   LEDDisplay0: LEDDisplay port map(number_out, done_signal, counter,
                                    segment_a, segment_b, segment_c,
                                    segment_d, segment_f, segment_g);
